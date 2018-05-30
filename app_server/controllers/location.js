fe689b89540ffe30fb61ae07f51a83da2963efd4
Shibasis Roy <rlinfocomm.shibasis@gmail.com>
2018-05-30 14:42:28 +0530

    Fix API URL

--- app_server/controllers/location.js
+++ app_server/controllers/location.js
@@ -4,7 +4,7 @@ var apiOptions = {
   server: "http://localhost:3000"
 };
 if(process.env.NODE_ENV === 'production'){
-  apiOptions.server = "https://loc8r-appjs.herokuapp.com/";
+  apiOptions.server = "https://loc8r-appjs.herokuapp.com";
 }
 
 var renderHomepage = function(req, res, responseBody){
